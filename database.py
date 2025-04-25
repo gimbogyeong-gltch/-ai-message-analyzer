b1ccbd11414bcd43d9d378e5b48175ece672a121
김보경 <gimbogyeong237@gmail.com>
2025-04-25 18:21:45 +0900

    fix: update .gitignore to include model outputs and data

--- database.py
+++
@@ -1,9 +0,0 @@
-# database.py
-class ClosenessDatabase:
-    def __init__(self):
-        # TODO: 실제 DB 연결 전에는 빈 생성자
-        pass
-
-    def get_closeness(self, user_id: str, target_id: str) -> int:
-        # TODO: 실제 친밀도 조회 전에는 항상 50점 리턴
-        return 50
